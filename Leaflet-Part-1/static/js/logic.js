6172e086ecc7616e69e003c1ea61d14a530cfac3
a-dhil <angaddeepdhillon@Angaddeeps-MacBook-Air.local>
2023-12-03 15:19:33 -0500

    tile layer added to my map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,32 +1,21 @@
-// Create the tile layer that will be the background of our map.
-//let streetmap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//    attribution: 'map data:&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// initialize a leaflet map
+let myMap=L.map("map",{
+  center:[11,224],
+  zoom:4
+})
 
-//});
-
-//{
-//    attribution:
-//      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-//  });
-
-// We create the tile layer that will be the background of our map.
-console.log("Step 1 working");
-
-// We create the tile layer that will be the background of our map.
+//create the title layer that will be the background of the map using OpenTopoMap tile.
 let basemap = L.tileLayer(
   "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
   {
     attribution:
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-  });
-let myMap=L.map("map",{
-    center:[11,224],
-    zoom:4
-});
-basemap.addTo(map);
+  }).addTo(myMap);
 
+// fetch the geo json data with D3
 let url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(url).then(function(response){
- 
+  features=response.features;
+  console.log(features);
 });
