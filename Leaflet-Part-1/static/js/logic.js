6ccbb4a5980a71806de77a719d3405b8e06661c9
a-dhil <angaddeepdhillon@Angaddeeps-MacBook-Air.local>
2023-12-03 21:15:52 -0500

    creating legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -18,42 +18,32 @@ let basemap=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // fetch the geo json data with D3
 let url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// d3.json(url).then(function(response){
-//   features=response.features;
-//   console.log(features);
-//   let marker_limit=features.length;
-//   for(let i=0; i<marker_limit; i++){
-//     let location=features[i].geometry;
-//     //console.log(location)
-//     if(location){
-//       L.marker(location.coordinates[1],location.coordinates[0]).addTo(myMap);
-//     }
-//   }
-// });
 //Function to map magnitude to marker size
 function getSize(magnitude) {
   const size = Math.sqrt(magnitude) * 4;
-  return isNaN(size) ? 4 : size; // Use a default size (5 in this case) if NaN
+  return isNaN(size) ? 4 : size; // Use a default size (4 in this case) if NaN
 }
 
 function getColor(depth) {
   let color=""
   if (depth < -10) {
-    return color= '#0008000';
+    return color= '#98ee00';
   } else if (depth >= -10 && depth < 10) {
-    return color= '#A3F600';
+    return color= '#d4ee00';
   } else if (depth >= 10 && depth < 30) {
-    return color= '#DCF400';
+    return color= '#ee9c00';
   } else if (depth >= 30 && depth < 50) {
-    return color= '#F7DB11';
+    return color= '#ee9c00';
   } else if (depth >= 50 && depth < 70) {
-    return color= '#FDB72A';
+    return color= '#ea2c2c';
   } else if (depth >= 70 && depth < 90) {
-    return color='#FCA35D';
+    return color='#ea2c2c';
   } else {
-    return color= '#FF5F65';
+    return color= '#ea2c2c';
   }
 }
+
+
 
 d3.json(url).then(function(response) {
   let features = response.features;
@@ -73,7 +63,24 @@ d3.json(url).then(function(response) {
         color: getColor(depth),
         fillColor:getColor(depth),
         fillOpacity:0.75,
-      }).addTo(myMap);
+      }).bindPopup(`<p>magnitude: ${magnitude}</p> <p> Depth: ${depth}</p> <p> Location: ${features[i].properties.place}</p>`).addTo(myMap);
     }
   }
 });
+let legend=L.control({
+  position:"bottomright"
+});
+
+ legend.onAdd=function(){
+   let div=L.DomUtil.create("div","info legend");
+   let grades=[-10,10,30,50,70,90];
+   let colors=['#98ee00','#d4ee00','#ee9c00','#ea822c','#ea2c2c'];
+   for(let i=0; i<grades.length;i++){
+     div.innerHTML +=
+       '<i style="background:' + colors[i] + '"></i> ' +
+       grades[i] + (grades[i + 1] ? '&ndash;' + (grades[i + 1] - 1) + '<br>' : '+');
+   }
+   return div;
+ };
+
+legend.addTo(myMap);
